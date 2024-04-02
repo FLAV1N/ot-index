1783f0e8bf6c12dc8457a407e19f51b653f5f6c4
FLAV1N <122433506+FLAV1N@users.noreply.github.com>
2024-04-02 15:06:21 +0700

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -17,7 +17,7 @@ module.exports = {
   title: "FLAV1N's Stash",
 
   // The folder that you are to share publicly with onedrive-cf-index-ng. Use '/' if you want to share your root folder.
-  baseDirectory: '/Anime/',
+  baseDirectory: '/Anime',
 
   // [OPTIONAL] This represents the maximum number of items that one directory lists, pagination supported.
   // Do note that this is limited up to 200 items by the upstream OneDrive API.
@@ -30,7 +30,7 @@ module.exports = {
   // googleFontMono - the monospace font used in onedrive-cf-index-ng.
   googleFontMono: 'JetBrains Mono',
   // googleFontLinks -  an array of links for referencing the google font assets.
-  googleFontLinks: ['https://fonts.googleapis.com/css2?family=JetBrains+Mono&family=Inter:wght@400;500;700&display=swap'],
+  googleFontLinks: ['https://fonts.googleapis.com/css2?family=JetBrains+Mono&family=Inter:wght@400;500;700&display=swap','https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap'],
 
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
@@ -39,7 +39,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/Private', '/Demo/😎Another Private Folder Password 123'],
+  protectedRoutes: ['/Apps', '/Attachments', 'subtitle'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: "",
@@ -49,19 +49,19 @@ module.exports = {
   // you provide. See the documentation for details.
   links: [
     {
-      name: '<i class="fa-solid fa-house"></i>Home',
+      name: 'Home',
       link: 'https://www.otakuturu.app/',
     },
     {
-      name: '<i class="fa-brands fa-discord"></i>Discord',
+      name: 'Discord',
       link: 'https://discord.com/invite/aRQZPDGP72',
     },
     {
-      name: '<img src="https://nyaa.si/static/favicon.png" alt="nyaa" style="height: 13px;vertical-align: middle;margin-right: 7px;position: relative;top: -2px;">Nyaa',
+      name: 'Nyaa',
       link: 'https://nyaa.si/?f=0&c=0_0&q=flav1n',
     },
     {
-      name: '<img src="https://www.perpusindo.info/favicon.ico" alt="Perpusindo" style="height: 13px;vertical-align: middle;margin-right: 7px;position: relative;top: -2px;">Perpusindo',
+      name: 'Perpusindo',
       link: 'https://www.perpusindo.info/user/FLAV1N',
     },
   ],
